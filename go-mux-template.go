21df3bccacf7c2786ac534571fe6c81edd0cd967
Yakhya Dabo <yakhyadabo@gmail.com>
2019-08-16 07:52:36 +0100

    [main] format

--- go-mux-template.go
+++ go-mux-template.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"github.com/yakhyadabo/go-mux-template/controller"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/yakhyadabo/go-mux-template/controller"
 )
 
 func main() {
@@ -12,10 +13,9 @@ func main() {
 
 	router.HandleFunc("/api/employees", controller.GetEmployees).Methods("GET")
 	router.HandleFunc("/api/employee", controller.CreateEmployee).Methods("POST")
-	router.HandleFunc("/api/employee", controller.GetEmployee).Queries("id", "{id}",).Methods("GET")
+	router.HandleFunc("/api/employee", controller.GetEmployee).Queries("id", "{id}").Methods("GET")
 
-
-	err := http.ListenAndServe(":" + "8000", router) //Launch the app, visit localhost:8000/api
+	err := http.ListenAndServe(":"+"8000", router) //Launch the app, visit localhost:8000/api
 	if err != nil {
 		log.Print(err)
 	}
